fcf14b0fba7204e0c24483df4d42b5231f2b9489
Jangmin Lee <jang.xmin@gmail.com>
2021-12-28 21:21:17 +0900

    fix: CORS 쿠키 공유 문제 해결

--- frontend/redux/sagas/rootSaga.js
+++ frontend/redux/sagas/rootSaga.js
@@ -5,6 +5,7 @@ import userSaga from "./user_saga";
 import postSaga from "./post_saga";
 
 axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.withCredentials = true; // 쿠키 사용
 
 export default function* rootSaga() {
   yield all([fork(userSaga), fork(postSaga)]);
